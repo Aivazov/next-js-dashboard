8c44315b06a411898c4f0a3884c90d7c092a7bee
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 15:21:40 +0200

    finishing tailwind TopCards

--- components/TopCards.tsx
+++ components/TopCards.tsx
@@ -4,22 +4,31 @@ export default function TopCards() {
   return (
     <div className="grid lg:grid-cols-5 gap-4 p-4">
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
-        <div className='flex flex-col w-full pb-4'>
-          <p className='text-2xl font-bold'>$7,000</p>
-          <p className='text-gray-600'>Daily Revenue</p>
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$7,000</p>
+          <p className="text-gray-600">Daily Revenue</p>
         </div>
+        <p className="bg-green-200 rounded-lg flex justify-center items-center p-2">
+          <span className="text-green-700 text-lg">+18%</span>
+        </p>
       </div>
       <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
-        <div className='flex flex-col w-full pb-4'>
-          <p className='text-2xl font-bold'>$7,000</p>
-          <p className='text-gray-600'>Daily Revenue</p>
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$547,000</p>
+          <p className="text-gray-600">YTD Revenue</p>
         </div>
+        <p className="bg-green-200 rounded-lg flex justify-center items-center p-2">
+          <span className="text-green-700 text-lg">+11%</span>
+        </p>
       </div>
       <div className=" bg-white flex justify-between w-full border p-4 rounded-lg">
-        <div className='flex flex-col w-full pb-4'>
-          <p className='text-2xl font-bold'>$7,000</p>
-          <p className='text-gray-600'>Daily Revenue</p>
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$51,000</p>
+          <p className="text-gray-600">Customers Revenue</p>
         </div>
+        <p className="bg-green-200 rounded-lg flex justify-center items-center p-2">
+          <span className="text-green-700 text-lg">+19%</span>
+        </p>
       </div>
     </div>
   );
