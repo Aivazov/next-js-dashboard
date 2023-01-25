ef15a4a171871f53357ad791a893b8f217229f59
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 17:29:11 +0200

    working on RecentOrders.tsx

--- components/RecentOrders.tsx
+++ components/RecentOrders.tsx
@@ -3,7 +3,22 @@ import { persons } from '../api/fakeAPI';
 import { FaShoppingBag } from 'react-icons/fa';
 
 export default function RecentOrders() {
-  return <div>
-    <FaShoppingBag />
-  </div>;
+  return (
+    <div className="w-full relative col-span-1 lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-y-scroll">
+      <h1>RecentOrders</h1>
+      <ul>
+        {persons.map((order, id) => (
+          <li
+            key={id}
+            className="cursor-pointer bg-gray-50 hover:bg-gray-100 my-2 p-3 rounded-lg flex items-center"
+          >
+            <div className="bg-purple-100 p-3 rounded-lg">
+              <FaShoppingBag className="text-purple-800" />
+            </div>
+          </li>
+        ))}
+      </ul>
+      {/* <FaShoppingBag /> */}
+    </div>
+  );
 }
