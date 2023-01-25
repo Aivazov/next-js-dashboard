95ea85401e5e4152c34e2933e642338a8a533738
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 16:43:13 +0200

    working on RecentOrders

--- components/RecentOrders.tsx
+++ components/RecentOrders.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { persons } from '../api/fakeAPI';
+import { FaShoppingBag } from 'react-icons/fa';
 
 export default function RecentOrders() {
-  return <div>RecentOrders</div>;
+  return <div>
+    <FaShoppingBag />
+  </div>;
 }
