a70f933a1ab1bf38a6c8f4bfdc0637b8dfdafe8f
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 17:50:31 +0200

    working on customers page

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs';
+import { persons } from '../api/fakeAPI';
 
 export default function customers() {
-  return (
-    <div>customers</div>
-  )
+  return <div>customers</div>;
 }
