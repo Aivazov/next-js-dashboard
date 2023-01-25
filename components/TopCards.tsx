6345e440d9e7327641d20dbdb7346cdbf3816c7e
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 15:11:56 +0200

    tailwind TopCards

--- components/TopCards.tsx
+++ components/TopCards.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export default function TopCards() {
   return (
-    <div className=''>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <div className="grid lg:grid-cols-5 gap-4 p-4">
+      <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className='flex flex-col w-full pb-4'>
+          <p className='text-2xl font-bold'>$7,000</p>
+          <p className='text-gray-600'>Daily Revenue</p>
+        </div>
+      </div>
+      <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className='flex flex-col w-full pb-4'>
+          <p className='text-2xl font-bold'>$7,000</p>
+          <p className='text-gray-600'>Daily Revenue</p>
+        </div>
+      </div>
+      <div className=" bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className='flex flex-col w-full pb-4'>
+          <p className='text-2xl font-bold'>$7,000</p>
+          <p className='text-gray-600'>Daily Revenue</p>
+        </div>
+      </div>
     </div>
   );
 }
