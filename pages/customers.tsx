283e420647030a1e66de67caf900cb00e5ff873b
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 17:56:16 +0200

    working on customers page

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Header from '../components/Header';
 import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs';
 import { persons } from '../api/fakeAPI';
 
 export default function customers() {
-  return <div>customers</div>;
+  return (
+    <div className="bg-gray-200 min-h-screen">
+      <div>
+        <Header />
+      </div>
+      <div className="p-4">
+        <div className="bg-white f-full m-auto p-4 border rounded-lg overflow-y-auto"></div>
+      </div>
+    </div>
+  );
 }
