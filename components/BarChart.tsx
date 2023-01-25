ff951e5ddec438cb143b3fe112b4ca116728dfd6
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-25 16:34:19 +0200

    adding diagramm

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -23,9 +23,24 @@ ChartJS.register(
 export default function BarChart() {
   const [chartData, setChartData] = useState({ datasets: [] });
   const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ['Mon', 'Tues', 'Wes', 'Thurs', 'Fri', 'Sat', 'Sun'],
+      datasets: [
+        {
+          label: 'Sales $',
+          data: [11945, 12551, 11233, 22994, 15423, 12123, 14785],
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.4)',
+        },
+      ],
+    });
+  }, []);
+
   return (
     <>
-      <div className="">
+      <div className="bg-white w-full rounded-lg md:col-span-2 relative m-auto border h-[50vh] lg:h-[70vh]">
         <Bar data={chartData} options={chartOptions} />
       </div>
     </>
