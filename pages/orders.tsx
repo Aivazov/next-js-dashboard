0330a88b7de752d90d3dd8dc7b3887daf5134429
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-28 00:37:17 +0200

    working on orders page

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Header from '../components/Header';
+import { persons } from '../api/fakeAPI';
 
 export default function orders() {
-  return <div>orders</div>;
+  const headerTitle = 'Orders';
+  return (
+    <div className="bg-gray-200 min-h-screen">
+      <div>
+        <Header title={headerTitle} />
+      </div>
+      <div className="p-4">
+        <div className="flex justify-between p-4 bg-white rounded-lg"></div>
+      </div>
+    </div>
+  );
 }
