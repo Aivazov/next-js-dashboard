fb5631b133d28b8a0f600953e96dedc35c202f6e
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-27 10:01:54 +0200

    customizing the customers page

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -11,8 +11,8 @@ export default function customers() {
         <Header title={headerTitle} />
       </div>
       <div className="p-4">
-        <div className="bg-white f-full m-auto p-4 border rounded-lg overflow-y-auto">
-          <div>
+        <div className="bg-white w-full m-auto p-4 border rounded-lg overflow-y-auto">
+          <div className="my-3 p-2 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
             <span>Name</span>
             <span>Status</span>
             <span>Date</span>
